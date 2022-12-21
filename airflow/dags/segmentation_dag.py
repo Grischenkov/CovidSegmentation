95a095a5521334bbcd5227a63a4efcaf05b631a5
Nikita Grishchenkov <nikita.grishenkov@gmail.com>
2022-12-22 02:23:54 +0300

    feat(segmentation): реализовать этап обучения модели

--- airflow/dags/segmentation_dag.py
+++ airflow/dags/segmentation_dag.py
@@ -4,6 +4,7 @@ import yaml
 import airflow
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from files import Files
 from dataset import Dataset
@@ -68,8 +69,85 @@ def clahe_segmentation_datasets():
         test_ct_images[i] = clahe.apply(test_ct_images[i])
     Files.save_images(train_ct_images, ct_train_df, 'data/segmentation/train/CT/')
     Files.save_images(test_ct_images, ct_test_df, 'data/segmentation/test/CT/')
+def generate_segmentation_datasets():
+    X_train = np.array(Files.load_images(pd.read_csv('data/segmentation/ct_train_df.csv'), 'data/segmentation/train/CT/')) / 255
+    Y_train = np.array(Files.load_images(pd.read_csv('data/segmentation/masks_train_df.csv'), 'data/segmentation/train/MASK/')) / 255
+    Files.save_npy('data/segmentation/train/X_train.npy', X_train)
+    Files.save_npy('data/segmentation/train/Y_train.npy', Y_train)
+    X_test = np.array(Files.load_images(pd.read_csv('data/segmentation/ct_test_df.csv'), 'data/segmentation/test/CT/')) / 255
+    Y_test = np.array(Files.load_images(pd.read_csv('data/segmentation/masks_test_df.csv'), 'data/segmentation/test/MASK/')) / 255
+    Files.save_npy('data/segmentation/test/X_test.npy', X_test)
+    Files.save_npy('data/segmentation/test/Y_test.npy', Y_test)
 def train_segmentation_model():
-    pass
+    parameters = yaml.safe_load(open('data/parameters.yaml'))
+    mlflow.set_tracking_uri('http://host.docker.internal:5000')
+    mlflow.set_experiment('segmentation')
+
+    X_train = Files.load_npy('data/segmentation/train/X_train.npy')
+    Y_train = Files.load_npy('data/segmentation/train/Y_train.npy')
+    X_test = Files.load_npy('data/segmentation/test/X_test.npy')
+    Y_test = Files.load_npy('data/segmentation/test/Y_test.npy')
+
+    model = segmentation_model.model()
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=parameters['segmentation']['learning_rate']), 
+            loss=segmentation_model.dice_loss, 
+            metrics=[segmentation_model.dice])
+
+    with mlflow.start_run(run_name=parameters['segmentation']['experiment_name']):
+        mlflow.log_param('CLAHE', parameters['segmentation']['use_clahe'])
+        mlflow.log_param('n_samples', parameters['segmentation']['n_samples'])
+        mlflow.log_param('test_size', parameters['segmentation']['test_size'])
+        mlflow.log_param('epochs', parameters['segmentation']['epochs'])
+        mlflow.log_param('batch', parameters['segmentation']['batch'])
+        mlflow.log_param('shuffle', parameters['segmentation']['shuffle'])
+        mlflow.log_param('optimizer', model.optimizer.name)
+        mlflow.log_param('learning_rate', parameters['segmentation']['learning_rate'])
+        
+        history = model.fit(X_train, Y_train, epochs=parameters['segmentation']['epochs'], shuffle=parameters['segmentation']['shuffle'])
+        
+        pred = list()
+        true = Y_test.tolist()
+        avg_dice = 0
+        for i in range(len(X_test)):
+            pred.append(model.predict(X_test[i].reshape(1, 512, 512, 1)).reshape(512, 512))
+            avg_dice += segmentation_model.dice(np.array(true[i]).astype('float32'), np.array(pred[i]).astype('float32'))
+        avg_dice /= len(X_test)
+        avg_dice = avg_dice.numpy()
+        mlflow.log_metric('avg test dice', avg_dice)
+        
+        for i in range(parameters['segmentation']['epochs']):
+            mlflow.log_metric('loss', history.history['loss'][i], step=i)
+            mlflow.log_metric('dice', history.history['dice'][i], step=i)
+        
+        with open('model_summary.txt', 'w') as file:
+            model.summary(print_fn=lambda x: file.write(x + '\n'))
+        mlflow.log_artifact('model_summary.txt')        
+        
+        indices = random.choices(range(len(X_test)), k=5)
+        fig, axes = plt.subplots(3, 5, figsize=(15,9))
+
+        for ii, idx in enumerate(indices) :
+            axes[0][ii].imshow(X_test[idx], cmap='bone')
+            axes[0][ii].set_title('CT image'); plt.grid(None)
+            axes[0][ii].set_xticks([]); axes[0][ii].set_yticks([])
+            
+            axes[1][ii].imshow(true[idx], cmap='gray')
+            axes[1][ii].set_title('COVID mask'); plt.grid(None)
+            axes[1][ii].set_xticks([]); axes[1][ii].set_yticks([])
+
+            axes[2][ii].imshow(pred[idx], cmap='gray')
+            axes[2][ii].set_title('Predicted COVID mask'); plt.grid(None)
+            axes[2][ii].set_xticks([]); axes[2][ii].set_yticks([])
+        fig.savefig('example_prediction.png')
+        mlflow.log_artifact('example_prediction.png')
+
+        mlflow.log_artifact('./plugins/segmentation_model.py')
+        
+        model.save('segmenter.h5')
+        mlflow.log_artifact('segmenter.h5')
+
+        mlflow.keras.log_model(keras_model=model, artifact_path='segmentation_models')
+        mlflow.end_run()
 
 with airflow.DAG(
     'segmentation',
